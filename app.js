664d6ffccc86ce7a43dcac7e6b61dcc210f5a356
unknown <nhut19091989@gmail.com>
2022-10-20 19:35:14 +0700

    tests

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
+$(document).ready(function(){
+    $('.about-img-thousand').slick({
+        slidesToShow: 3,
+        infinite: true,
+        slidesToScroll: 1,
+        // autoplay: true,
+        autoplaySpeed: 1000,    
+        prevArrow:`<button type='button' class='slick-prev slick-arrow'><ion-icon name="chevron-back-circle-outline"></ion-icon></button>`,
+        nextArrow:`<button type='button' class='slick-next slick-arrow'><ion-icon name="chevron-forward-circle-outline"></ion-icon></button>`,
+       dots: true, // 1 2 3 4 
+       responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+            
+            
+          }
+        }
+      ]
+    });
+  });   
 
+
+ 
